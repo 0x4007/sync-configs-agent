f46d0417c91a2ba98e100fd2ed710ae6a97e91b3
アレクサンダー.eth <gpg@pavlovcik.com>
2024-10-19 07:09:45 +0900

    feat: update workflow to use editor_instruction

--- src/sync-configs/sync-configs-non-interactive.ts
+++ src/sync-configs/sync-configs-non-interactive.ts
@@ -1,10 +1,10 @@
 import { processRepositories } from "./process-repositories";
 
 export async function syncConfigsNonInteractive() {
-  const instruction = process.env.INPUT_STRING;
+  const instruction = process.env.EDITOR_INSTRUCTION;
   if (!instruction) {
     throw new Error(
-      "No instruction provided. You need to pass in an instruction either as command-line arguments or through the INPUT_STRING environment variable."
+      "No instruction provided. You need to pass in an instruction either as command-line arguments or through the EDITOR_INSTRUCTION environment variable."
     );
   }
   await processRepositories(instruction, true);
